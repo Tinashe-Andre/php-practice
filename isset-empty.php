85abf107055bf4a3b7bb2b6157cd22bac7712ab1
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-10-24 01:02:33 +0200

    Practicing using the isset and empty functions

---
+++ isset-empty.php
@@ -0,0 +1,13 @@
+<?php
+    // isset() = Returns TRUE if a variable is declared and not null
+    // empty() = Returns TRUE if a variable is not declared, false, null, ""
+
+    $username = "TDre";
+
+    if(isset($username)){
+        echo"This variable is set";
+    }
+    else{
+        echo"This variable is NOT set";
+    }
+?>
