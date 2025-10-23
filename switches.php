f6c5259d5529b578e19e83828dbc9b8a683adf81
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-10-23 21:57:49 +0200

    Update switches.php

--- switches.php
+++ switches.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Switches</title>
 </head>
 <body>
-    <h2>Garde Checker</h2>
+    <h2>Grade Checker</h2>
     <?php
     //switch = replacement to using many elseif statements 
     //                  more efficient, less code to write
