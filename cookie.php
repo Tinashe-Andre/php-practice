73aff1e041428568c8b6ef18677f66126eb8ea97
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-10-25 18:12:15 +0200

    Creating cookies with php

---
+++ cookie.php
@@ -0,0 +1,5 @@
+<?php
+    // cookie = Information about a user stored in a user's web-browser 
+    //          targeted advertisements, browsing preferences, and 
+    //          other non-sensitive data
+?>
