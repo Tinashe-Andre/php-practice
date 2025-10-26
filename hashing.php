45ded686809b961e3c749735adb22f579d77edc1
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-10-26 17:05:24 +0200

    Update hashing.php

--- hashing.php
+++ hashing.php
@@ -3,5 +3,14 @@
     //           into letters, numbers, and/or symbols 
     //           via a mathematical process. (similar to encryption) 
     //           Hides the original data from 3rd parties.
-    
+
+    $password = "pizza123";
+
+    $hash = password_hash("password122", PASSWORD_DEFAULT);
+
+    if(password_verify($password, $hash)){
+        echo"You are logged in!";
+    } else{
+        echo"Incorrect password!";
+    }
 ?>
