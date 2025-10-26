57396de567a3314e1144f42a2abe852c9d3f7319
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-10-26 16:43:08 +0200

    Practicing hashing input

---
+++ hashing.php
@@ -0,0 +1,7 @@
+<?php
+    // hashing = transforming sensitive data (password) 
+    //           into letters, numbers, and/or symbols 
+    //           via a mathematical process. (similar to encryption) 
+    //           Hides the original data from 3rd parties.
+    
+?>
